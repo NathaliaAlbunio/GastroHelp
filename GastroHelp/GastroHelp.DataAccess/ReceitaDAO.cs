887c81c2e01fd1e03667ed6c0c890c5fb57f0160
Tiago Andrade <tgnandrade@gmail.com>
2018-03-22 09:06:53 -0300

    classes
    Identification: 3
    Change-Id: I05e3d557d41e057f17b89782c486f568c84af7df

--- GastroHelp/GastroHelp.DataAccess/ReceitaDAO.cs
+++ GastroHelp/GastroHelp.DataAccess/ReceitaDAO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GastroHelp.Models
+namespace GastroHelp.DataAccess
 {
-    public class Class1
+    public class ReceitaDAO
     {
     }
 }
--- GastroHelp/GastroHelp.Models/Receita.cs
+++ GastroHelp/GastroHelp.Models/Receita.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GastroHelp.DataAccess
+namespace GastroHelp.Models
 {
-    public class Class1
+    public class Receita
     {
     }
 }
